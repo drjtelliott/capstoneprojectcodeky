10be42465fe01d8d74a1e7b266e81e79644c760a
J.T. Elliott <drjtelliott@gmail.com>
2023-12-27 13:24:44 -0800

    add two CSV files for TKO and FOX

--- README.md
+++ README.md
@@ -3,3 +3,4 @@ First draft of capstone project.
 Created a branch.
 Python 3.12
 yfinance 0.2.33
+Download the download.py to gather data from the data folder.
--- download.py
+++ download.py
@@ -1,23 +1,26 @@
 import yfinance as yf
+import pandas as pd
 
 # reference for download: https://github.com/ranaroussi/yfinance/wiki/Tickers#download
 
-def download_data(ticker:str, start: str, end: str) -> any: 
+def download_data(ticker:str, start: str, end: str) -> pd.DataFrame: 
+    print("Downloading {ticker} data....")
+
     # using yfinance download ticker information for particular date range
     data = yf.download(ticker, start=start, end=end)
 
     # update data frame to only contain column we care about
     data = data.drop(columns=['Open', 'High', 'Low', 'Adj Close'])
 
+    print("...{ticker} data downloaded")
+
     # return the pandas dataframe
     return data
 
-# download fox data  
-print("FOX DATA") 
+# download fox data   
 fox_data = download_data("FOX", start="2023-01-01", end="2023-12-22")
-print(fox_data)
+fox_data.to_csv('data/fox_data.csv', index=False) 
 
-# download fox data  
-print("TKO DATA") 
+# download fox data   
 tko_data = download_data("TKO", start="2023-01-01", end="2023-12-22")
-print(tko_data)
+tko_data.to_csv('data/tko_data.csv', index=False) 
