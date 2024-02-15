3fa4be31dd1b342d80ef2e4423e0e5df9340f7d8
J.T. Elliott <drjtelliott@gmail.com>
2024-02-15 14:48:36 -0800

    Added merged data of two csv files into data frame (df)

--- prepare_data.py
+++ prepare_data.py
@@ -32,28 +32,42 @@ def clean_data(data: pd.DataFrame) -> pd.DataFrame:
 
     return data
 
-def prepare_data(ticker:str, start: str, end: str, dir_name:str): 
+def prepare_data(ticker:str, start: str, end: str, dir_name: str) -> pd.DataFrame: 
     print(f"Preparing data for {ticker}")
 
     data = download_data(ticker, start=start, end=end)
     data = clean_data(data)
 
     data.to_csv(f'{dir_name}/{ticker.lower()}_data.csv') 
+    
+    return data
 
+def get_merged_data(ticker1: str, ticker2: str, start: str, end: str, dir_name: str) -> pd.DataFrame:
+    # Prepare data for both tickers this also saves the data to a csv file
+    ticker1_data = prepare_data(ticker1, start, end, dir_name)
+    ticker2_data = prepare_data(ticker2, start, end, dir_name)
+ 
+    # Rename the 'Close' column in each dataframe to avoid confusion after merge
+    df1 = ticker1_data.rename(columns={'Close': f'close_{ticker1.lower()}'})
+    df2 = ticker2_data.rename(columns={'Close': f'close_{ticker2.lower()}'})
+
+    # Merge the dataframes on 'Date'
+    merged_df = pd.merge(df1, df2, on='Date', how='inner')
+
+    return merged_df
+  
 start_date="2019-10-01"
 end_date="2023-12-31"
 
-#Define the directory name
+# Define the directory name
 dir_name = 'data'
 
-#Check is directory is already exisiting
+# Check if the directory already exists
 if not os.path.exists(dir_name):
-    print(f'Creating Directory {dir_name}')
-    #If not create the directory
+    print(f'Creating directory {dir_name}')
+    # If not, create the directory
     os.makedirs(dir_name)
 
-# download fox data   
-prepare_data("FOX", start=start_date, end=end_date, dir_name=dir_name) 
 
-# download fox data   
-prepare_data("TKO", start=start_date, end=end_date, dir_name=dir_name)
+merged_df = get_merged_data('FOX', 'TKO', '2019-10-01', '2023-12-31', 'data')
+print(merged_df)
